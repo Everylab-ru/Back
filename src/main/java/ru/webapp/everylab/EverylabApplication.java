dccfa5f90fd5439638f1d581d8b16ec1ce645171
Nezoxxxd <bets070614@gmail.com>
2024-09-10 22:13:41 +0300

    VLB-6 Create empty project

---
+++ src/main/java/ru/webapp/everylab/EverylabApplication.java
@@ -0,0 +1,13 @@
+package ru.webapp.everylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverylabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverylabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/webapp/everylab/EverylabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webapp.everylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverylabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
